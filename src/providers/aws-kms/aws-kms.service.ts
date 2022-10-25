599738823ca7d2ea441fe2bdb000f3a74a5701de
olvresc <oliver@ubx.ph>
2022-10-25 12:15:11 +0800

    Add eip712 sign typed data

--- src/providers/aws-kms/aws-kms.service.ts
+++ src/providers/aws-kms/aws-kms.service.ts
@@ -12,7 +12,7 @@ import {
   findEthereumSig,
   determineCorrectV,
 } from './aws-kms.util';
-import { ethers } from 'ethers';
+import { ethers, TypedDataDomain, TypedDataField } from 'ethers';
 import {
   RawTransactionOptions,
   serializeRawTransaction,
@@ -45,7 +45,7 @@ export class AWSKMSService {
     const command = new GetPublicKeyCommand({ KeyId: keyId });
     const result = await this.awsKmsClient.send(command);
     // Convert Uint8Array to Buffer Array
-    // AWS KMS from aws-sdk is returning Buffer;
+    // AWS KMS from aws-sdk is returning Buffer
     // Which is required for finding the ethereum signature
     return Buffer.from(result.PublicKey);
   }
@@ -55,9 +55,46 @@ export class AWSKMSService {
     keyId: string,
   ): Promise<string> {
     const serializedRawTransaction = serializeRawTransaction(transaction);
-    const digestBuffer = Buffer.from(
-      ethers.utils.arrayify(ethers.utils.keccak256(serializedRawTransaction)),
+    const disgestString = ethers.utils.keccak256(serializedRawTransaction);
+    const transactionSignature = await this._signDigest(disgestString, keyId);
+
+    return ethers.utils.serializeTransaction(
+      // Pass the raw transaction/not serialized with the signature
+      ethers.utils.parseTransaction(serializedRawTransaction),
+      transactionSignature,
     );
+  }
+
+  async getEthAddressByKeyId(keyId: string): Promise<string> {
+    const publicKey = await this.getPublicKey(keyId);
+    return getEthereumAddress(publicKey);
+  }
+
+  async signTypedData(
+    domain: TypedDataDomain,
+    types: Record<string, Array<TypedDataField>>,
+    message: Record<string, any>,
+    keyId: string, // The signer of the hashed data
+  ): Promise<Record<string, any>> {
+    const hash = ethers.utils._TypedDataEncoder.hash(domain, types, message);
+    const signature = await this._signDigest(hash, keyId);
+
+    return {
+      signature,
+      signerAddress: ethers.utils.verifyTypedData(
+        domain,
+        types,
+        message,
+        signature,
+      ),
+    };
+  }
+
+  private async _signDigest(
+    digestString: string,
+    keyId: string,
+  ): Promise<string> {
+    const digestBuffer = Buffer.from(ethers.utils.arrayify(digestString));
     const command = new SignCommand({
       KeyId: keyId,
       Message: digestBuffer,
@@ -70,30 +107,21 @@ export class AWSKMSService {
     // AWS KMS from aws-sdk is returning Buffer;
     // Which is required for finding the ethereum signature
     const signatureBuffer = Buffer.from(result.Signature);
-    const signature = findEthereumSig(signatureBuffer);
+    const _signature = findEthereumSig(signatureBuffer);
     // Admin burner, minter or user wallet address if for token approval
     const signerAddress = await this.getEthAddressByKeyId(keyId);
     const { v } = determineCorrectV(
       digestBuffer,
-      signature.r,
-      signature.s,
+      _signature.r,
+      _signature.s,
       signerAddress,
     );
-    const transactionSignature = ethers.utils.joinSignature({
+    const signature = ethers.utils.joinSignature({
       v,
-      r: `0x${signature.r.toString('hex')}`,
-      s: `0x${signature.s.toString('hex')}`,
+      r: `0x${_signature.r.toString('hex')}`,
+      s: `0x${_signature.s.toString('hex')}`,
     });
 
-    return ethers.utils.serializeTransaction(
-      // Pass the raw transaction/not serialized with the signature
-      ethers.utils.parseTransaction(serializedRawTransaction),
-      transactionSignature,
-    );
-  }
-
-  async getEthAddressByKeyId(keyId: string): Promise<string> {
-    const publicKey = await this.getPublicKey(keyId);
-    return getEthereumAddress(publicKey);
+    return signature; // Can be slit to check the correct signer address
   }
 }
